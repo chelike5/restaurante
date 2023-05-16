491d8281a968bd136b94944c01704548a5c3e78a
José Villasmil <jevillaster@gmail.com>
2023-05-16 01:13:04 -0400

    first commit

---
+++ restaurant.js
@@ -0,0 +1,28 @@
+
+
+
+function mostrarHamburg(){
+    document.getElementById('menuhamburg').style.display ='';
+    document.getElementById('menuPizzas').style.display = 'none';
+    document.getElementById('menutequeños').style.display = 'none';
+    document.getElementById('postres').style.display = 'none';
+  
+  }
+  
+  function mostrarPizza(){
+    document.getElementById('menuhamburg').style.display ='none';
+    document.getElementById('menuPizzas').style.display = 'block';
+    document.getElementById('menutequeños').style.display = 'none';
+    document.getElementById('postres').style.display = 'none';
+  
+  }
+  
+  function mostrarTequeños(){
+    document.getElementById('menuhamburg').style.display ='none';
+    document.getElementById('menuPizzas').style.display = 'none';
+    document.getElementById('menutequeños').style.display = 'block';
+    document.getElementById('postres').style.display = 'none';
+  
+  }
+  
+ 
